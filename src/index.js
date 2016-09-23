a78035779bb2d727c42f4f30eb29bf1057cd9c2e
Martin Burdon <martin@martinburdon.co.uk>
2016-09-23 14:35:19 +0100

    Getting balance

--- src/index.js
+++ src/index.js
@@ -31,7 +31,8 @@
 
   function authoriseUser() {
     var clientId = 'oauthclient_00009CaBEXgA33A8vbCqQL';
-    var redirectUri = 'http://localhost:8888/Apps/monzo-playground/index.html';
+    // var redirectUri = 'http://localhost:8888/Apps/monzo-playground/index.html';
+    var redirectUri = 'http://localhost:8888/monzo-playground/index.html';
     var stateToken = 'xcvbngfdtr5y67iuyjhm';
     var url = "https://auth.getmondo.co.uk/?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code&state=" + stateToken;
     window.open(url);
@@ -39,7 +40,8 @@
 
   function getAccessToken(authorizationCode) {
     var clientId = 'oauthclient_00009CaBEXgA33A8vbCqQL';
-    var redirectUri = 'http://localhost:8888/Apps/monzo-playground/index.html';
+    // var redirectUri = 'http://localhost:8888/Apps/monzo-playground/index.html';
+    var redirectUri = 'http://localhost:8888/monzo-playground/index.html';
     var clientSecret = 'WXIXJDX1zNpWUFvpjRtFrT1/T/X/NFx1JUQfIE4HNYF/6L0nON4An5ciMMlbcEM0nYAy5Kgvtpc+75F8alzk';
     var url = "https://api.monzo.com/oauth2/token";
 
@@ -53,30 +55,52 @@
         redirect_uri: redirectUri,
         code: authorizationCode
       }
+    };
+
+    request(options, function (error, response, body) {
+      console.log(error);
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+        // requestBalance(body);
+        requestAccountDetails(body);
+      }
+    });
+  };
+
+  function requestAccountDetails(data) {
+    var dat = JSON.parse(data);
+    console.log(dat);
+
+    var url = "https://api.monzo.com/accounts";
+
+    var options = {
+      uri: url,
+      headers: {
+        Authorization: 'Bearer ' + dat.access_token
+      }
     }
 
     request(options, function (error, response, body) {
       console.log(error);
       if (!error && response.statusCode == 200) {
         console.log(body) // Show the HTML for the Google homepage.
-        requestAccounts(body);
+        requestBalance(body, dat.access_token);
       }
     });
   };
 
-  function requestAccounts(data) {
+  function requestBalance(data, accessToken) {
     var dat = JSON.parse(data);
-    console.log(dat);
-    console.log('hi ', dat.access_token);
+    var userId = dat.accounts[0].id;
     var url = "https://api.monzo.com/balance";
 
     var options = {
       uri: url,
       headers: {
-        Authorization: 'Bearer ' + dat.access_token
+        Authorization: 'Bearer ' + accessToken
       },
       qs: {
-        account_id: dat.user_id
+        account_id: userId
       }
     }
 
